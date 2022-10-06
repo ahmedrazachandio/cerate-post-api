a4ba42d93cd8ed291b45acb0420146514a85844a
AhmedRaza <86877851+ahmedrazachandio@users.noreply.github.com>
2022-10-06 10:54:20 +0500

    code push

--- post-api/src/App.js
+++ post-api/src/App.js
@@ -1,9 +1,28 @@
+import {useState , useEffect} from "react";
+
 import logo from './logo.svg';
+import Post from './components/post'
 import './App.css';
 
 function App() {
+  useEffect(() =>{
+    axios.get("https://my-json-server.typicode.com/minzamammalik/jsonplaceholder/posts")
+      .then(response => {
+        console.log("response :", response);
+      })
+      .catch(err => {
+        console.log("error :", err);
+
+      })
+
+  },[])
+
+
+
+
   return (
     <div className="App">
+      <Post name="Ahmed Raza" /> 
       
     </div>
   );
