49c5a3990d3354af2a496cde5381cc9f3e23b089
AhmedRaza <86877851+ahmedrazachandio@users.noreply.github.com>
2022-10-06 12:46:14 +0500

    push

--- post-api/src/App.js
+++ post-api/src/App.js
@@ -1,14 +1,18 @@
-import {useState , useEffect} from "react";
-
+import {useState ,useEffect } from "react";
+import axios from "axios";
 import logo from './logo.svg';
 import Post from './components/post'
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
   useEffect(() =>{
     axios.get("https://my-json-server.typicode.com/minzamammalik/jsonplaceholder/posts")
       .then(response => {
-        console.log("response :", response);
+        console.log("response :", response.data);
+        setPosts(response.data);
+
       })
       .catch(err => {
         console.log("error :", err);
@@ -22,7 +26,16 @@ function App() {
 
   return (
     <div className="App">
-      <Post name="Ahmed Raza" /> 
+
+      <Post name="Ahmed Raza" />
+      // {
+      //   posts.map((eachPost , i)=>{
+
+      //     <>
+      //         <Post name={eachPost.name}></Post>
+      //     </>
+      //   })
+      // }
       
     </div>
   );
